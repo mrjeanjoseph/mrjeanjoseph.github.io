1aa8a9af0435fe73c48603465a7da3cd8ca77b2b
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2021-09-06 16:36:10 -0400

    restructuring the portfolio porj

--- scr/js/master_config.js
+++ scr/js/master_config.js
@@ -1,8 +1,14 @@
-let projects = [
+let portfolioProjects = [
     {
         projectName: "RestaurantAgogo",
         ProjectDescription: "Select a random restaurant from a list based on zipcode for you to go to when you can't decide where to eat.",
         TechnologyToolsUsed: "ASP.NET Core, MS/SQL hosted on Azure with Angular.",
-        projectURLAddress: ""
+        projectURLAddress: "https://github.com/mrjeanjoseph/RestaurantAGoGo"
+    },
+    {
+        projectName: "ServiceDesk CRM",
+        ProjectDescription: "Develop helpdesk ticketing software application for documentation of client request and resolution",
+        TechnologyToolsUsed:"ASP.NET Core, MS/SQL with frontend Angular.",
+        projectURLAddress: "https://github.com/mrjeanjoseph/ServiceDeskCRM"
     }
-]
+];
