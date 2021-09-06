5eb8d9b1788fefb8365cfca417e198611efe4a23
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2021-09-06 16:27:46 -0400

    restructuring the portfolio porj

---
+++ scr/js/master_config.js
@@ -0,0 +1,8 @@
+let projects = [
+    {
+        projectName: "RestaurantAgogo",
+        ProjectDescription: "Select a random restaurant from a list based on zipcode for you to go to when you can't decide where to eat.",
+        TechnologyToolsUsed: "ASP.NET Core, MS/SQL hosted on Azure with Angular.",
+        projectURLAddress: ""
+    }
+]
