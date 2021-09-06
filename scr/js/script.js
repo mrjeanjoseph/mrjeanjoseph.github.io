478b08459465b073b0588e750af8747016f80aae
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2021-09-06 16:48:20 -0400

    restructuring the portfolio porj

--- scr/js/script.js
+++ scr/js/script.js
@@ -1,4 +1,14 @@
+//
+'use strick';
 
 const items = document.getElementsByClassName('item');
 items[2].classList.add('d-none');
 console.log(items);
+
+
+let objectOne = {
+    nameOne: "Jeff",
+    Occupation: "Conductor"
+};
+
+console.log(portfolioProjects);
