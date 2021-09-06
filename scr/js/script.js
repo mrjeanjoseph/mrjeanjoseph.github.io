138396cbc7637fa5065d83e1f0da0fd5014f3cbe
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2021-09-06 16:57:42 -0400

    restructuring the portfolio porj

--- scr/js/script.js
+++ scr/js/script.js
@@ -1,14 +1,10 @@
 //
 'use strick';
 
-const items = document.getElementsByClassName('item');
-items[2].classList.add('d-none');
-console.log(items);
-
-
-let objectOne = {
-    nameOne: "Jeff",
-    Occupation: "Conductor"
-};
-
-console.log(portfolioProjects);
+//Header section
+let myName = "Frednel Jean-Joseph",
+    MyTitle = "Software Developer",
+    inMyToolBelt = "jQuery, Node.JS, PL/SQL";
+document.getElementById("myName").innerHTML = myName;
+document.getElementById("MyTitle").innerHTML = MyTitle;
+document.getElementById("inMyToolBelt").innerHTML = inMyToolBelt;
