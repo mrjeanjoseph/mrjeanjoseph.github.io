ebfeca9ef75d20d20c2accdeb6400105e8b40700
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2021-10-01 18:45:46 -0400

    making updates

--- scr/js/master_config.js
+++ scr/js/master_config.js
@@ -1,14 +1,30 @@
 let portfolioProjects = [
     {
-        projectName: "RestaurantAgogo",
+        projectName: "Restaurant Agogo",
         ProjectDescription: "Select a random restaurant from a list based on zipcode for you to go to when you can't decide where to eat.",
+        projectImageDisplay: "",        
         TechnologyToolsUsed: "ASP.NET Core, MS/SQL hosted on Azure with Angular.",
         projectURLAddress: "https://github.com/mrjeanjoseph/RestaurantAGoGo"
     },
     {
         projectName: "ServiceDesk CRM",
         ProjectDescription: "Develop helpdesk ticketing software application for documentation of client request and resolution",
+        projectImageDisplay: "",
         TechnologyToolsUsed:"ASP.NET Core, MS/SQL with frontend Angular.",
         projectURLAddress: "https://github.com/mrjeanjoseph/ServiceDeskCRM"
-    }
+    },
+    {
+        projectName: "Personal Project",
+        ProjectDescription: "Project created to showcase my understanding and expertise in web development",
+        projectImageDisplay: "",
+        TechnologyToolsUsed:"HTML, CSS and JavaScript.",
+        projectURLAddress: "https://github.com/mrjeanjoseph/ServiceDeskCRM"
+    },
+    {
+        projectName: "ServiceDesk CRM",
+        ProjectDescription: "Develop helpdesk ticketing software application for documentation of client request and resolution",
+        projectImageDisplay: "",
+        TechnologyToolsUsed:"ASP.NET Core, MS/SQL with frontend Angular.",
+        projectURLAddress: "https://github.com/mrjeanjoseph/ServiceDeskCRM"
+    },
 ];
