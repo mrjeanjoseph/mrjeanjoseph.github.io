896c762053deb3e6e20b2c4c8480c15bac964d2e
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2023-10-01 11:44:47 -0400

    made additional updates

--- src/js/content.js
+++ src/js/content.js
@@ -3,38 +3,43 @@
 const portfolioProjects = [
     {
         projectName: "Restaurant Agogo",
-        ProjectDescription: "Select a random restaurant based on zipcode and decide where to eat.",
+        ProjectShortDesc: "Select a random restaurant based on zipcode and decide where to eat.",
+        ProjectLongDesc: "Project description to be added here",
         projectImageDisplay: "/src/img/img1.png",        
         TechnologyToolsUsed: "ASP.NET Core, MS/SQL hosted on Azure with Angular 12.",
         projectURLAddress: "https://github.com/mrjeanjoseph/RestaurantAGoGo"
     },
     {
         projectName: "ServiceDesk CRM",
-        ProjectDescription: "Helpdesk ticketing software application for request and resolution.",
+        ProjectShortDesc: "Helpdesk ticketing software application for request and resolution.",
+        ProjectLongDesc: "Project description to be added here",
         projectImageDisplay: "/src/img/img2.png",
         TechnologyToolsUsed:"ASP.NET Core, MS/SQL with frontend Angular 12.",
         projectURLAddress: "https://github.com/mrjeanjoseph/ServiceDeskCRM"
     },
     {
         projectName: "Shopping List App",
-        ProjectDescription: "Shopping List App to keep track of items purchased.",
+        ProjectShortDesc: "Shopping List App to keep track of items purchased.",
+        ProjectLongDesc: "Project description to be added here",
         projectImageDisplay: "/src/img/img3.png",
         TechnologyToolsUsed:"JavaScript, jQuery and IndexDB.",
         projectURLAddress: "https://deepvuecrud.github.io/shoppinglist/"
     },
     {
         projectName: "Budget Tracker",
-        ProjectDescription: "Budget Tracker app to monitor expenses and deposits.",
+        ProjectShortDesc: "Budget Tracker app to monitor expenses and deposits.",
+        ProjectLongDesc: "Project description to be added here",
         projectImageDisplay: "/src/img/img4.png",
         TechnologyToolsUsed:"HTML, CSS and JavaScript.",
         projectURLAddress: "https://deepvuecrud.github.io/budget-tracker/"
     },
     {
-        projectName: "Currently Under Planning",
-        ProjectDescription: "Projects in the planning phase are still being planned and will be addressed when other projects are completed.",
+        projectName: "In Planning Phase",
+        ProjectShortDesc: "Projects in the planning phase are still being planned and will be addressed when other projects are completed.",
+        ProjectLongDesc: "Project description to be added here",
         projectImageDisplay: "/src/img/planning.jpg",
         TechnologyToolsUsed:"HTML, CSS and JavaScript.",
-        projectURLAddress: "/"
+        projectURLAddress: ""
     },
 ];
 
@@ -50,21 +55,28 @@ $("#inMyToolBelt").text(introduction.currentStacks);
 
 //Generate all the projects
 $.each(portfolioProjects, function(index, item) {
-    const projects = `
+
+    if(item.projectName.includes("Planning Phase")){
+        console.log($(this));
+        console.log($(this).parent());
+        $(this).css("background-color", 'red')
+    }
+    
+    const projectLists = `
     <div class="item">
         <div class="card">
-            <a class="" target="_blank" href="${item.projectURLAddress}"><img
+            <a class="projectImg" target="_blank" href="${item.projectURLAddress}"><img
                     src=" ${item.projectImageDisplay}" width="400" height="175" />
             </a>
             <div class="content">
-                <h1><a target="_blank"></a>${item.projectName}</h1>
-                <p>${item.ProjectDescription}</p>
+                <h1>${item.projectName}</h1>
+                <p>${item.ProjectShortDesc}</p>
                 <p>Tool & Framework used: ${item.TechnologyToolsUsed}</p>
-                <p><a target="_blank" href="${item.projectURLAddress}">Click here for more details</a></p>
+                <p><a href="${item.projectURLAddress}">Click here for more details</a></p>
             </div>
         </div>
     </div>`;
-    $("#projectViews").append(projects);
+    $("#displayAllProjects").append(projectLists);
 });
 
 //Footer
